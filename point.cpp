23393b235e68a3a976c34b479e9dfef05fdf2ab8
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-03 10:33:46 +0530

    updated

--- point.cpp
+++ point.cpp
@@ -1,7 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    cout<<"Jai Shree Ram"<<endl;
+    int a = 10;
+    cout << "Address of a: " << &a; // Prints the memory address of 'a'
+    int *ptr = &a;
+    cout<<"The Address of a stored in pointer: "<<ptr<<endl;
+    //Take example of char
+
+    // const char* c = "Jai Shree Ram";
+    // char *ptr1 = &c;//here the adress of char c is stored in ptr1 using pointer.
+    // cout<<"The pointer of char is: "<<ptr1<<endl;
 }
